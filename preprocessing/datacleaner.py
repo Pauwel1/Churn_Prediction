61bcc5ea5dfb83d942aea6ff38963ac150a5aa60
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-08-05 10:14:38 +0200

    rename data_analysis
    to datacleaner

--- preprocessing/datacleaner.py
+++ preprocessing/datacleaner.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import numpy as np
 
+# import dataframe
 crunch = pd.read_csv("assets/BankChurners.csv")
+
+# data cleaning
+# drop last two biassed columns
+crunch = crunch[crunch.columns[:-2]]
+
+# change dtypes
+crunch["Gender"] = crunch["Gender"].astype(str)
 
 print("dtypes: \n", crunch.dtypes)
 print("__________")
-print(crunch.isnull().sum())
+
 print("__________")
 print(crunch["Gender"].value_counts().sum())
