a54927bf2796f495b8b7cbe869f6ad7ea68f0fb5
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-08-05 12:34:15 +0200

    dummies created

--- preprocessing/datacleaner.py
+++ preprocessing/datacleaner.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import numpy as np
+from pandas.core.series import Series
 
 # import dataframe
 crunch = pd.read_csv("assets/BankChurners.csv")
 
 # data cleaning
-# drop last two biassed columns
+# drop columns that are not necessary
 crunch = crunch[crunch.columns[:-2]]
+crunch = crunch.drop("CLIENTNUM", axis = 1)
 
-# change dtypes
-crunch["Gender"] = crunch["Gender"].astype(str)
+# determine target and features
+y = crunch["Attrition_Flag"].to_numpy()
+X = crunch.drop("Attrition_Flag", axis = 1)
 
-print("dtypes: \n", crunch.dtypes)
-print("__________")
+# create dummies
+cat_columns = X.select_dtypes(include = ['object'])
 
-print("__________")
-print(crunch["Gender"].value_counts().sum())
+for item in cat_columns:
+    dummies = pd.get_dummies(X[item], columns = cat_columns.columns)
+    X = pd.concat([X, dummies], axis = 1)
+    del X[item]
+
+print(X.columns)
