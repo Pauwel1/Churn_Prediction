9f0ebb91c1f25a236d7c77be3c90b4fe32d0cd62
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-08-05 10:02:17 +0200

    looking at the data

--- preprocessing/Data_Analysis.py
+++ preprocessing/Data_Analysis.py
@@ -2,5 +2,9 @@ import pandas as pd
 import numpy as np
 
 crunch = pd.read_csv("assets/BankChurners.csv")
-print(crunch.column_names)
 
+print("dtypes: \n", crunch.dtypes)
+print("__________")
+print(crunch.isnull().sum())
+print("__________")
+print(crunch["Gender"].value_counts().sum())
