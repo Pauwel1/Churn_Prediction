b79fcaf60237e2336a8a08f1968b8af2d37ecb6d
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-08-09 16:28:54 +0200

    K-means-clusterer

--- preprocessing/clustering.py
+++ preprocessing/clustering.py
@@ -1,50 +1,38 @@
 import pandas as pd
-from numpy import unique, where
+import numpy as np
 from datacleaner import dataCleaner
-from sklearn.cluster import AffinityPropagation, AgglomerativeClustering
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 churn = pd.read_csv("assets/BankChurners.csv")
 X, y = dataCleaner(churn)
+print(X.columns)
 
-def affinityPropagator(X):
-    # define and fit the model
-    model = AffinityPropagation(max_iter = 250, damping = 0.9, random_state = None).fit(X)
-    cluster_centers_indices = model.cluster_centers_indices_
-    n_clusters_ = len(cluster_centers_indices)
-    # assign a cluster to each example
-    yhat = model.predict(X)
-    # retrieve unique clusters
-    clusters = unique(yhat)
+def kmClusterer(X):
+    # Check out how many clusters are desirable
+    # WCSS = "Within Cluster Sum of Squares"
+    # k = number of clusters
+    wcss = []
+    # Choose a range between 1 and 10 clusters
+    for k in range(1,11):
+        kmeans = KMeans(n_clusters = k, init = "k-means++").fit(X)
+        wcss.append(kmeans.inertia_)
+    # plot the figure: where the "bend in the elbow" is, 
+    # that number of clusters is desirable
+    plt.figure(figsize=(12,6))    
+    plt.grid()
+    plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
+    plt.xlabel("K Value")
+    plt.xticks(np.arange(1,11,1))
+    plt.ylabel("WCSS")
+    plt.savefig("assets/elbow_method.png")
+    plt.clf()
 
-    # create scatter plot for samples from each cluster
-    for cluster in clusters:
-        # get row indexes for samples with this cluster
-        row_ix = where(yhat == cluster)
-        # create scatter of these samples
-        plt.scatter(X.loc[row_ix, 0], X.loc[row_ix, 1])
+    km = KMeans(n_clusters = 2)
+    clusters = km.fit_predict(X)
+    X["label"] = clusters
 
-    # plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c = yhat)
 
-    # show the plot
-    plt.title(f'Estimated number of clusters = {n_clusters_}')
-    plt.legend()
-    plt.show()
 
-aff = affinityPropagator(X)
-
-# def agglomerativeClusterer(X):
-#     # define the model
-#     model = AgglomerativeClustering(n_clusters = 2)
-#     # fit model and predict clusters
-#     yhat = model.fit_predict(X, y)
-#     # retrieve unique clusters
-#     clusters = unique(yhat)
-#     # create scatter plot for samples from each cluster
-#     for cluster in clusters:
-#         # get row indexes for samples with this cluster
-#         row_ix = where(yhat == cluster)
-#         # create scatter of these samples
-#         plt.scatter(X[row_ix, 0], X[row_ix, 1])
-#     # show the plot
-#     plt.show()
+clusterer(X)
